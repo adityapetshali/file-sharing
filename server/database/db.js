debca29977aab87580fe0a0e277cffee4450043f
adityapetshali <adityapetshali@outlook.com>
2024-04-06 16:35:31 -0700

    minor changes in data base config

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 const DBConnection =async () => {
-    const MONODB_URI=`mongodb://aditya:I5YxVZYJUiqWQkon@ac-rwrw0ob-shard-00-00.lniihyl.mongodb.net:27017,ac-rwrw0ob-shard-00-01.lniihyl.mongodb.net:27017,ac-rwrw0ob-shard-00-02.lniihyl.mongodb.net:27017/?ssl=true&replicaSet=atlas-s1pvcu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=file-sharing`;
+    const MONODB_URI=`mongodb://aditya:aCB1VVBwUJ2K1Apr@ac-6bhtyha-shard-00-00.ryoeovz.mongodb.net:27017,ac-6bhtyha-shard-00-01.ryoeovz.mongodb.net:27017,ac-6bhtyha-shard-00-02.ryoeovz.mongodb.net:27017/?ssl=true&replicaSet=atlas-v8h21g-shard-0&authSource=admin&retryWrites=true&w=majority&appName=file-sharing`;
     try{
         await mongoose.connect(MONODB_URI, {useNewUrlParser: true});
         console.log('database connected');
